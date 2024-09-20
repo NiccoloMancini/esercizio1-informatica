f2e65a1ee4c9dd7d3a46517ef88ce28cffdf5459
Niccolo Mancini <niccolomancini3@gmail.com>
2024-09-20 10:26:36 +0200

    fine esercizio tasso di idratazione

---
+++ Tasso-di-Idratazione/tasso idratazione.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Niccolò Mancini</h1>
+    <?php
+        $peso = 65;
+        $risultato = $peso * 30;
+        echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $risultato</p>"
+    ?>
+</body>
+</html>
